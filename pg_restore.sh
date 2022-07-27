a4708dd0c91683b2062dd9ded6e6f4bf1807b174
Kemper <fkemper@mail.ru>
2022-07-27 07:51:57 +0300

    Adding checks to pg_restore_internal Change policies in share_slow_include

--- pg_restore.sh
+++ pg_restore.sh
@@ -1,10 +1,16 @@
 
-SrvFrom=p0sgbp-pg2001lv.vtb24.ru
-SrvTo=ltsgbp-pgc001lv.test.vtb.ru
+#SrvFrom=p0smib-pg5005lp.region.vtb.ru
+#SrvTo=ltsmib-pg5009lp.test.vtb.ru
+SrvFrom=p0smib-pg5006lp.region.vtb.ru
+SrvTo=ltsmib-pg5011lp.test.vtb.ru
+
 SrvMedia=ubkp-nbu514l.test.vtb.ru
-Bckp=pgbackup_p0sgbp-pg2001lv.vtb24.ru_13.07.2022-02.05
+#SrvMedia=ubkp-nbu214l.region.vtb.ru
+#Bckp=pgbackup_p0smib-pg5005lp.region.vtb.ru_21.07.2022-02.05
+Bckp=pgbackup_p0smib-pg5006lp.region.vtb.ru_21.07.2022-02.05
+
 DirRnm=pg_data
 
-./pg_restore_internal $SrvFrom $SrvTo $SrvMedia $Bckp $DirRnm
+/root/bin/pg_restore_internal $SrvFrom $SrvTo $SrvMedia $Bckp #$DirRnm
 
 
