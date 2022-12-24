5d4a58123b86ff73ee2bb19c38e22d56ea471502
Kemper <fkemper@mail.ru>
2022-12-24 18:34:06 +0300

    pg_restore_internal: add 2>/dev/null

--- pg_restore_sched.sh
+++ pg_restore_sched.sh
@@ -1,1 +1,2 @@
-/opt/VRTSnbu/root/bin/pg_restore_sched_int.sh > /opt/VRTSnbu/root/bin/pg_restore_sched_in_$$.log 2>&1
+# v 0.1.0
+/opt/VRTSnbu/root/bin/pg_restore_sched_int.sh > /opt/VRTSnbu/root/bin/pg_restore_sched_int_$$.log 2>&1
